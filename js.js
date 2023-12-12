bb459de1944d2e263be16105705c7f1f3d050f23
telin <143735191+hjhjhkjklklklkl@users.noreply.github.com>
2023-12-12 20:19:16 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+var nav_3 = document.getElementById('in_up');
+var icon = document.getElementById('bar');
+
+icon.addEventListener('click',(event)=>{
+    nav_3.classList.toggle('hide')
+    event.stopPropagation()
+})
+
+document.addEventListener('click',(event)=>{
+    if(nav_3.classList.contains('hide')){
+        nav_3.classList.remove('hide')
+    }
+    event.stopPropagation()
+})
